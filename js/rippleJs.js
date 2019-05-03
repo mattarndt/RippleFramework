269cdd9d8305ef3dbdc0896401589d569be86dd3
Matt Arndt <mattarndt22@gmail.com>
2019-05-03 01:17:56 -0400

    first commit

--- js/rippleJs.js
+++ js/rippleJs.js
@@ -1,22 +1,14 @@
 /* A framework created by Matthew Arndt*/
-
-//this looks for all classes that have rippleJs in the class name
-var o = document.getElementsByClassName("rippleJs");
-var i;
-//runs the ripple function for every element that has rippleJs in the class name
-for (i = 0; i < o.length; i++) {
-    Ripple(o[i]);
-}
-/**
- * Adds Ripple effect to objects 
- * @param {Object}  o - object we want to add ripple effect to
- */
 "use strict"
 
-function Ripple(obj) {
-    
+
+function Ripple(id, duration) {
+    var obj = document.getElementById(id);
     //Set defaults 
-    var duration = 500;
+    console.log(duration);
+    if(duration===undefined){
+        duration = 500;
+    }
     var color = 'rgba(255,255,255,0.5)';
     //add an click event to objects
     obj.addEventListener('click', function (e) {
